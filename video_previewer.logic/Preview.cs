e7d488aad912652fe5c1ef93bb063326113cddda
evgen.fesuk <evgen.fesuk@gmail.com>
2017-11-03 20:38:11 +0200

    evpty app

---
+++ video_previewer.logic/Preview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace video_previewer.logic
+{
+    public class Preview
+    {
+    }
+}
